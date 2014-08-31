8245277a54ebb8efac05ec7c3588e45fb9cb2fcf
Aaron <aaron@example.com>
2014-08-30 22:24:03 -0400

    Added registers to CPU class.

--- src/csci_6461_group_project/CPU.java
+++ src/csci_6461_group_project/CPU.java
@@ -1,5 +1,29 @@
 package csci_6461_group_project;
 
-public class CPU {
+import java.util.BitSet;
 
+public class CPU extends Computer {
+	
+	//4 General Purpose Registers(GPRs)
+	private BitSet R0 = new BitSet(18);
+	private BitSet R1 = new BitSet(18);
+	private BitSet R2 = new BitSet(18);
+	private BitSet R3 = new BitSet(18);
+	
+	//3 Index Registers
+	private BitSet X1 = new BitSet(12);
+	private BitSet X2 = new BitSet(12);
+	private BitSet X3 = new BitSet(12);
+	
+	//Special registers
+	private BitSet PC = new BitSet(12);
+	private BitSet CC = new BitSet(4);
+	private BitSet IR = new BitSet(18);
+	private BitSet MAR = new BitSet(12);
+	private BitSet MBR = new BitSet(18);
+	private BitSet MSR = new BitSet(18);
+	private BitSet MFR = new BitSet(4);
+	
+	//Other registers we deemed necessary to add
+	
 }
