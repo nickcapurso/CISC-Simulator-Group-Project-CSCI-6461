fb8a36a644489f60ef53d2bbb70af06f166d4cd2
nickcapurso <nickcapurso@gwmail.gwu.edu>
2014-09-09 18:18:39 -0400

    Added Load/Store instructions to Context.

--- src/edu/gwu/seas/csci/Context.java
+++ src/edu/gwu/seas/csci/Context.java
@@ -29,16 +29,31 @@ public class Context {
     private Map<Byte, String> opCodeStrings = new HashMap<Byte, String>();
 
     private Context() {
+    opcodeClasses.put("LDR", InstructionClass.LD_STR);
+    opcodeClasses.put("STR", InstructionClass.LD_STR);
+    opcodeClasses.put("LDA", InstructionClass.LD_STR);
+    opcodeClasses.put("LDX", InstructionClass.LD_STR);
+    opcodeClasses.put("STX", InstructionClass.LD_STR);
 	opcodeClasses.put("AMR", InstructionClass.LD_STR);
 	opcodeClasses.put("SMR", InstructionClass.LD_STR);
 	opcodeClasses.put("AIR", InstructionClass.LD_STR_IMD);
 	opcodeClasses.put("SIR", InstructionClass.LD_STR_IMD);
 
+	opCodeBytes.put("LDR", OpCodesList.LDR);
+	opCodeBytes.put("STR", OpCodesList.STR);
+	opCodeBytes.put("LDA", OpCodesList.LDA);
+	opCodeBytes.put("LDX", OpCodesList.LDX);
+	opCodeBytes.put("STX", OpCodesList.STX);
 	opCodeBytes.put("AMR", OpCodesList.AMR);
 	opCodeBytes.put("SMR", OpCodesList.SMR);
 	opCodeBytes.put("AIR", OpCodesList.AIR);
 	opCodeBytes.put("SIR", OpCodesList.SIR);
 
+	opCodeStrings.put(OpCodesList.LDR, "LDR");
+	opCodeStrings.put(OpCodesList.STR, "STR");
+	opCodeStrings.put(OpCodesList.LDA, "LDA");
+	opCodeStrings.put(OpCodesList.LDX, "LDX");
+	opCodeStrings.put(OpCodesList.STX, "STX");
 	opCodeStrings.put(OpCodesList.AMR, "AMR");
 	opCodeStrings.put(OpCodesList.SMR, "SMR");
 	opCodeStrings.put(OpCodesList.AIR, "AIR");
